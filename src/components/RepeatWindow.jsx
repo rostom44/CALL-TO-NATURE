e5b3d8801ff81432fab654f2a3e1a0a27b7410da
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-05-17 10:21:24 +0200

    Final Window is done

--- src/components/RepeatWindow.jsx
+++ src/components/RepeatWindow.jsx
@@ -2,11 +2,12 @@ import "./RepeatWindow.css";
 import { useState, useEffect } from "react";
 import data from "../assets/images_mignon.json";
 
-export default function RepeatWindow() {
+function RepeatWindow() {
   const [counter, SetCounter] = useState(0);
   const [topWindow, SetTopWindow] = useState(250);
   const [leftWindow, SetLeftWindow] = useState(500);
   const [timeRemaining, setTimeRemaining] = useState(1800);
+  const [counterDone , setCounterDone] = useState(false)
 
   function handleClick() {
     if (counter !== data.data.length - 1) {
@@ -16,7 +17,9 @@ export default function RepeatWindow() {
     SetTopWindow(Math.random() * 523);
     SetLeftWindow(Math.random() * 960);
     setTimeRemaining(timeRemaining + 5);
-
+    if ( counter === data.data.length - 1 ){
+      setCounterDone(true);
+    }
     // Play audio when the close button is clicked
     const audio = new Audio(data.data[counter].sound_cool);
     audio.play();
@@ -32,10 +35,9 @@ export default function RepeatWindow() {
     return () => clearTimeout(timer);
   }, [timeRemaining]);
 
-  console.log(counter);
   return (
     <>
-      <div className="window" style={{ top: topWindow, left: leftWindow }}>
+      {counterDone === false && <div className="window" style={{ top: topWindow, left: leftWindow }}>
         <div className="topWindow">
           <p>Call to Nature - ne réponds pas</p>
           <button type="button" alt="counterButton" onClick={handleClick}>
@@ -45,7 +47,11 @@ export default function RepeatWindow() {
         <img alt={data.data[counter].index} src={data.data[counter].lien} />
         <p>{data.data[counter].citation}</p>
         <button>JE FAIS MA PAUSE</button>
-      </div>
+      </div>}
+      {counterDone === true && <div className="gotoPause" style={{top: "36vh" , left: "33vw" }}>
+        <img alt="Angry Cat" src="../src/assets/angry cat.jpeg"/>
+        <p>LE JEU EST TERMINE VA RESPIRER !!!</p>
+        </div>}
       <div className="secondTimer">
         <p>Temps restant : </p>
         <p>
@@ -55,3 +61,6 @@ export default function RepeatWindow() {
     </>
   );
 }
+
+
+export default RepeatWindow;
